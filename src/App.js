fd25b8c7029100854824a44cfb2222b2bfbc4571
bevannutakor <bevannutakor@icloud.com>
2023-02-19 14:47:20 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Motivational Quote Generator </h1>
+      <div className="quote-container">
+          <h3 className="quote">"The future belongs to those who believe in the beauty of their dreams."</h3>
+          <p className="author">- Eleanor Roosevelt</p>
+      </div>
+      <button className="quote-button">Generate New Quote</button>
     </div>
   );
 }
